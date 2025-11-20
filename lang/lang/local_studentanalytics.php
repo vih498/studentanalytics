30eb037baae21f57a2550c123a20f1581682d3c0
Víh Albuquerque <ti.vih498@outlook.com>
2025-11-20 00:05:05 -0300

    Delete lang/lang directory

--- lang/lang/local_studentanalytics.php
+++
@@ -1,7 +0,0 @@
-<?php
-// English strings for local_studentanalytics
-
-$string['pluginname'] = 'Student Analytics';
-$string['privacy:metadata'] = 'This plugin does not store any personal data.';
-$string['page_title'] = 'Student Analytics Dashboard';
-$string['csv_error'] = 'Error processing the CSV file.';
