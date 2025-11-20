a736906e1ac7041c996333911abe1a37d2e4698f
Víh Albuquerque <ti.vih498@outlook.com>
2025-11-20 00:04:22 -0300

    Add files via upload

---
+++ lang/lang/local_studentanalytics.php
@@ -0,0 +1,7 @@
+<?php
+// English strings for local_studentanalytics
+
+$string['pluginname'] = 'Student Analytics';
+$string['privacy:metadata'] = 'This plugin does not store any personal data.';
+$string['page_title'] = 'Student Analytics Dashboard';
+$string['csv_error'] = 'Error processing the CSV file.';
